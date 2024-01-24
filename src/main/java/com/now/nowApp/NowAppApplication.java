3dfe54453a084832e56093e6250282595bcd42d4
Leandro <leamgon@gmail.com>
2024-01-24 19:48:41 -0300

    Fist commit

---
+++ src/main/java/com/now/nowApp/NowAppApplication.java
@@ -0,0 +1,13 @@
+package com.now.nowApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/now/nowApp/NowAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.now.nowApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
